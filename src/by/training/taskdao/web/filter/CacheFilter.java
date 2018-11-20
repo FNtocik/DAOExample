54628d8a0bede5631b799b59b822b92dff3005dc
FNtocik <pugachevzaz@gmail.com>
2018-11-20 09:42:06 +0300

    Added cache filter to prevent site caching

---
+++ src/by/training/taskdao/web/filter/CacheFilter.java
@@ -0,0 +1,28 @@
+package by.training.taskdao.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CacheFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse resp = (HttpServletResponse)servletResponse;
+        resp.setHeader("Cache-Control", "no-cache");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("Cache-Control", "no-store");
+        resp.setHeader("Cache-Control", "must-revalidate");
+        resp.setDateHeader("Expires", 0);
+        filterChain.doFilter(servletRequest, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
